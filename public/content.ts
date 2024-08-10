22765d6e41d98bcab14825f189eacd80792732f0
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-08-10 17:05:23 +0100

    update `content.ts` file

--- public/content.ts
+++ public/content.ts
@@ -5,26 +5,28 @@ export const content: string = `<div class="bg-l-silver-6 h-1/1 ins">
         <ul class="s-y-4">
             <li class="ai-c d-f t-d-silver-6">
                 <svg class="dim-6 fs-0 tc-pink" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="currentColor">
-                    <path d="M5 13L9 17L19 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <path d="M5 13L9 17L19 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                    </path>
                 </svg>
                 <p class="ml-2">Integration with Yumma CSS.</p>
             </li>
             <li class="ai-c d-f t-d-silver-6">
                 <svg class="dim-6 fs-0 tc-pink" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="currentColor">
-                    <path d="M5 13L9 17L19 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                </svg>
-                <p class="ml-2">Auto-completion.</p>
-            </li>
-            <li class="ai-c d-f t-d-silver-6">
-                <svg class="dim-6 fs-0 tc-pink" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="currentColor">
-                    <path d="M5 13L9 17L19 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <path d="M5 13L9 17L19 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                    </path>
                 </svg>
                 <p class="ml-2">Real-time preview changes.</p>
             </li>
-            <p class="t-d-silver-6">Have fun playing around and happy hacking!</p>
+            <li class="ai-c d-f t-d-silver-6">
+                <svg class="dim-6 fs-0 tc-pink" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="currentColor">
+                    <path d="M5 13L9 17L19 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                    </path>
+                </svg>
+                <p class="ml-2">Syntax highlight.</p>
+            </li>
             <div class="b-1 bc-l-silver-6"></div>
             <p class="fw-500 t-d-silver-6">To learn more about Yumma CSS</p>
-            <a class="fw-500 tc-pink" href="https://www.yummacss.com/" rel="noopener noreferrer">Read the docs! ↗</a>
+            <a class="fw-500 tc-pink" href="https://www.yummacss.com/" rel="noopener noreferrer" target="_blank">Read the docs! ↗</a>
         </ul>
     </div>
 </div>
