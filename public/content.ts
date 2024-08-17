e0a575630bc7787884537b9310d7c985ed1316b5
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-08-17 18:44:28 +0100

    update `content.ts` file

--- public/content.ts
+++ public/content.ts
@@ -1,33 +1,84 @@
-export const content: string = `<div class="bg-l-silver-6 h-1/1 ins">
-    <div class="b-1 bc-l-silver-4 bg-white bs-xs p-12">
-        <h2 class="tc-pink">Yumma CSS Play</h2>
-        <p class="mb-6 t-d-silver-6">Here's some of the features Yumma CSS Play offers:</p>
-        <ul class="s-y-4">
-            <li class="ai-c d-f t-d-silver-6">
-                <svg class="dim-6 fs-0 tc-pink" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="currentColor">
-                    <path d="M5 13L9 17L19 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                    </path>
-                </svg>
-                <p class="ml-2">Integration with Yumma CSS.</p>
-            </li>
-            <li class="ai-c d-f t-d-silver-6">
-                <svg class="dim-6 fs-0 tc-pink" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="currentColor">
-                    <path d="M5 13L9 17L19 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                    </path>
-                </svg>
-                <p class="ml-2">Real-time preview changes.</p>
-            </li>
-            <li class="ai-c d-f t-d-silver-6">
-                <svg class="dim-6 fs-0 tc-pink" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="currentColor">
-                    <path d="M5 13L9 17L19 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                    </path>
-                </svg>
-                <p class="ml-2">Syntax highlight.</p>
-            </li>
-            <div class="b-1 bc-l-silver-6"></div>
-            <p class="fw-500 t-d-silver-6">To learn more about Yumma CSS</p>
-            <a class="fw-500 tc-pink" href="https://www.yummacss.com/" rel="noopener noreferrer" target="_blank">Read the docs! ↗</a>
-        </ul>
+export const content: string = `
+<header class="bb-1 bc-l-silver-6 bg-white d-f fs-sm fw-w py-4 sm:fw-nw sm:jc-fs w-full">
+    <nav class="mx-auto px-4 sm:ai-c sm:d-f sm:jc-sb w-full">
+        <div class="ai-c d-f jc-sb">
+            <a class="f-none fs-xl fw-600" href="#">Yumma OS</a>
+            <div class="sm:d-none">
+                <button class="c-p ai-c b-1 bc-l-silver-6 bg-white bs-sm cg-2 d-if jc-c p-2 rad-2 tc-d-silver-6">
+                    <svg class="dim-4 fs-0" width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3 5H21" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M3 12H21" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M3 19H21" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+        <div class="d-none fb-full fg-1 ovf-h sm:d-b">
+            <div class="d-f fd-c g-5 sm:ai-c sm:fd-r sm:jc-fe sm:pl-5">
+                <a class="fw-500 tc-pink" href="#">Docs</a>
+                <a class="fw-500 h:tc-black tc-lead" href="#">Changelog</a>
+                <a class="fw-500 h:tc-black tc-lead" href="#">Blog</a>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<div class="m-4">
+    <a class="bg-pink bs-md d-b p-3 rad-2 ta-c" href="#">
+        <p class="d-ib fs-sm mx-1 tc-white">We're excited to announce the release of Yumma OS 7.2!</p>
+        <span class="ai-c c-p d-if fs-sm fw-700 cg-2 jc-c tc-white">What's new? <svg class="dim-5 fs-0" width="24px" height="24px" stroke-width="1.5"
+                viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9 6L15 12L9 18" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            </svg>
+        </span>
+    </a>
+</div>
+
+<div class="b-1 h-1/1 m-4 rad-2 bc-l-silver-6">
+  <div class="f-1 f-auto fd-c h-1/1 ins md:p-5 p-4">
+        <p class="fs-sm mt-2 tc-l-lead-4">Add your content here.</p>
     </div>
+</div>
+
+<div class="d-g">
+    <footer class="bc-l-silver-6 bg-white bt-1 lg:px-8 mt-auto mx-auto px-4 py-10 sm:px-6 w-full">
+        <div class="d-g g-6 gtc-2 lg:gtc-5 mb-10 md:gtc-4">
+            <div class="d-none lg:d-b">
+                <a class="fs-xl fw-600" href="#">Yumma OS</a>
+                <p class="fs-xs mt-3 sm:fs-sm tc-gray">© 2024 Example.</p>
+            </div>
+            <div>
+                <h4 class="fs-xs fw-600 tc-d-gray-6">PRODUCTS</h4>
+                <div class="fs-sm mt-3 s-y-3">
+                    <p><a class="h:tc-d-gray-4 tc-gray" href="#">API</a></p>
+                    <p><a class="h:tc-d-gray-4 tc-gray" href="#">Integrations</a></p>
+                </div>
+            </div>
+            <div>
+                <h4 class="fs-xs fw-600 tc-d-gray-6">SOLUTIONS</h4>
+                <div class="fs-sm mt-3 s-y-3">
+                    <p><a class="h:tc-d-gray-4 tc-gray" href="#">Marketing</a></p>
+                    <p><a class="h:tc-d-gray-4 tc-gray" href="#">Support</a></p>
+                    <p><a class="h:tc-d-gray-4 tc-gray" href="#">Documentation</a></p>
+                </div>
+            </div>
+            <div>
+                <h4 class="fs-xs fw-600 tc-d-gray-6">RESOURCES</h4>
+                <div class="fs-sm mt-3 s-y-3">
+                    <p><a class="h:tc-d-gray-4 tc-gray" href="#">Blog</a></p>
+                    <p><a class="h:tc-d-gray-4 tc-gray" href="#">Help center</a></p>
+                    <p><a class="h:tc-d-gray-4 tc-gray" href="#">Status</a></p>
+                </div>
+            </div>
+            <div>
+                <h4 class="fs-xs fw-600 tc-d-gray-6">DEVELOPERS</h4>
+                <div class="fs-sm mt-3 s-y-3">
+                    <p><a class="h:tc-d-gray-4 tc-gray" href="#">Get started</a></p>
+                    <p><a class="tc-gray h:tc-d-gray-4" href="#">API docs</a> <span class="tc-pink">— We're hiring</span></p>
+                    <p><a class="h:tc-d-gray-4 tc-gray" href="#">Community</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
 </div>
 `;
