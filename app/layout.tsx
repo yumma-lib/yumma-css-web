cfae484ac37f91abcd5d6acb7ca01967ea213995
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-08-27 16:22:39 +0100

    chore [feat]:
    - move `logo.png` to `/assets`
    - delete `yummacss.png`
    - add Apple Touch Icon support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import { Inter } from "next/font/google";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 import "./styles/globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Yumma CSS Play",
   description: "Yumma CSS Playground based on Sandpack.",
+  icons: {
+    apple: "/app/assets/img/logo-200.png"
+  }
 };
 
 export default function RootLayout({
