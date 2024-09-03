a115274d73abcb295927345b868fdff6063e980f
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-03 16:43:14 +0100

    add completion support

--- app/components/play.tsx
+++ app/components/play.tsx
@@ -1,12 +1,16 @@
 import { pageContent } from "./constants/content";
 import { Sandpack } from "@codesandbox/sandpack-react";
 import customTheme from "../theme";
+import { autocompletion } from "@codemirror/autocomplete";
 
 const Playground: React.FC = () => {
   return (
     <Sandpack
       template="static"
       options={{
+        codeEditor: {
+          extensions: [autocompletion()],
+        },
         externalResources: [
           "https://cdn.jsdelivr.net/gh/yumma-lib/yumma-css@2.0.0/dist/yumma.min.css",
         ],
