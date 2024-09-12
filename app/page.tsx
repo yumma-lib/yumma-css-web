1796a24dcf3ca223a551bdf02641c79c3188ab6e
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-12 14:00:28 +0100

    - add `beforeunload` listener in `page.tsx` - fix broken image path in `nav.tsx` component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 "use client";
+import { useEffect } from "react";
 import Navbar from "./components/nav";
 import Playground from "./components/play";
 
 const Home = () => {
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      event.preventDefault();
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
