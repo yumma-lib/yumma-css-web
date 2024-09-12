85c8cb093c7f55d0dd969eb1b1e8471043364508
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-12 14:12:22 +0100

    - remove `beforeunload` listener in `page.tsx`

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,8 @@
 "use client";
-import { useEffect } from "react";
 import Navbar from "./components/nav";
 import Playground from "./components/play";
 
 const Home = () => {
-  useEffect(() => {
-    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
-      event.preventDefault();
-      event.returnValue = "";
-    };
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, []);
-
   return (
     <>
       <Navbar />
