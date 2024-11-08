3c64a8085cd6bde5d8de07d93baecf404aad968c
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-11-08 23:49:29 +0000

    update image alt

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,19 +3,10 @@ import Logo from "../../public/assets/logo.png";
 
 const Navbar = () => {
   return (
-    <header
-      className="d-f fs-sm fw-w py-1 sm:fw-nw sm:jc-fs w-full"
-      style={{ backgroundColor: "#1f212a" }}
-    >
+    <header className="d-f fs-sm fw-w py-1 sm:fw-nw sm:jc-fs w-full" style={{ backgroundColor: "#1f212a" }}>
       <nav className="mx-auto px-4 sm:ai-c sm:d-f sm:jc-sb w-full">
         <div className="ai-c d-f jc-sb">
-          <Image
-            alt="Logo"
-            className="h-auto"
-            height={140}
-            src={Logo}
-            width={140}
-          />
+          <Image alt="Yumma CSS Play Logo" className="h-auto" height={140} src={Logo} width={140} />
         </div>
       </nav>
     </header>
