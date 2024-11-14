bffb58031983c89c11e0212be7c6508f56daf358
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-11-14 01:06:36 +0000

    update `content.ts` file

--- app/constants/content.ts
+++ app/constants/content.ts
@@ -1,4 +1,4 @@
 export const pageContent: string = `<div class="fd-c h-1/1 ins">
-    <h1 class="fs-md fw-600 tc-pink">Yumma Playground</h1>
-    <p class="fs-sm fw-400 tc-gray">Use the left panel to edit to this page</p>
+    <h1 class="fs-sm fw-600 tc-pink">Yumma Playground</h1>
+    <p class="fs-b fw-400 tc-gray">Use the left panel to edit to this page</p>
 </div>`;
