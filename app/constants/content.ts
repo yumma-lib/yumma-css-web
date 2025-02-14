c4c0e3d2bf62ffd0aa270e5987465b8def70634e
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-02-14 14:13:24 +0000

    fix: update defaultCode structure for improved readability in content.ts

--- app/constants/content.ts
+++ app/constants/content.ts
@@ -1,7 +1,11 @@
 export const defaultCode: string = `<div class="ai-c d-f fd-c h-1/1 jc-c">
+  <div class="mx-4 ta-c">
     <h1 class="ff-c fs-md tc-black">Try the latest version of Yumma CSS</h1>
-    <p class="fs-b tc-lead">A practical CSS framework with an abbreviated naming convention.</p>
-</div>`;
+    <p class="fs-b tc-lead">
+      A practical CSS framework with an abbreviated naming convention.
+    </p>
+  </div>
+</div>;`;
 
 export const defaultStyles: string = `@import url("https://cdn.jsdelivr.net/gh/yumma-lib/yumma-css@latest/dist/yumma.css");
 
