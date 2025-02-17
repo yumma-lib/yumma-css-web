3cf75f3f9fdf33339eeef2298bca00f349859c34
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-02-17 20:00:28 +0000

    feat: implement mobile-responsive Playground component with preview toggle

--- app/components/play.tsx
+++ app/components/play.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { autocompletion } from "@codemirror/autocomplete";
 import { completionSource } from "../completions";
 import { customCmTheme } from "../themes/cmTheme";
@@ -7,6 +7,7 @@ import { keymap } from "@codemirror/view";
 import { searchKeymap } from "@codemirror/search";
 import customSpTheme from "../themes/spTheme";
 import {
+  Sandpack,
   SandpackProvider,
   SandpackLayout,
   SandpackPreview,
@@ -15,7 +16,14 @@ import {
 } from "@codesandbox/sandpack-react";
 
 const Playground: React.FC = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth < 768);
   const [showPreview, setShowPreview] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <SandpackProvider
@@ -23,52 +31,54 @@ const Playground: React.FC = () => {
       template="static"
       theme={customSpTheme}
       options={{ externalResources: ["/styles.css"] }}>
-      <div className="md:d-none d-f jc-c p-2">
-        <button className="tc-white" onClick={() => {}}>
-          Show Editor / Show Preview
-        </button>
-      </div>
-
-      <SandpackLayout>
-        <div className="md:d-none">
-          {showPreview ? (
-            <SandpackPreview
-              showOpenInCodeSandbox={false}
-              showRefreshButton={false}
-              style={{ height: "calc(100dvh - 6rem)" }}
-              title="Preview"
-            />
-          ) : (
-            <SandpackCodeEditor
-              closableTabs
-              extensions={[autocompletion({ override: [completionSource] }), customCmTheme, keymap.of(searchKeymap)]}
-              showInlineErrors
-              showLineNumbers
-              showTabs
-              wrapContent
-              style={{ height: "calc(100dvh - 6rem)" }}
-            />
-          )}
-        </div>
-        <SandpackFileExplorer className="d-none md:d-b" />
-        <SandpackCodeEditor
-          closableTabs
-          extensions={[autocompletion({ override: [completionSource] }), customCmTheme, keymap.of(searchKeymap)]}
-          showInlineErrors
-          showLineNumbers
-          showTabs
-          wrapContent
-          className="d-none md:d-b"
-          style={{ height: "calc(100dvh - 4rem)" }}
-        />
-        <SandpackPreview
-          showOpenInCodeSandbox={false}
-          showRefreshButton={false}
-          className="d-none md:d-b"
-          style={{ height: "calc(100dvh - 4rem)" }}
-          title="Preview"
-        />
-      </SandpackLayout>
+      {isMobile ? (
+        <>
+          <div className="d-f jc-c p-2">
+            <button className="tc-white" onClick={() => setShowPreview((prev) => !prev)}>
+              {showPreview ? "Show Editor" : "Show Preview"}
+            </button>
+          </div>
+          <SandpackLayout>
+            {showPreview ? (
+              <SandpackPreview
+                showOpenInCodeSandbox={false}
+                showRefreshButton={false}
+                style={{ height: "calc(100dvh - 6rem)" }}
+                title="Preview"
+              />
+            ) : (
+              <SandpackCodeEditor
+                closableTabs
+                extensions={[autocompletion({ override: [completionSource] }), customCmTheme, keymap.of(searchKeymap)]}
+                showInlineErrors
+                showLineNumbers
+                showTabs
+                wrapContent
+                style={{ height: "calc(100dvh - 6rem)" }}
+              />
+            )}
+          </SandpackLayout>
+        </>
+      ) : (
+        <SandpackLayout>
+          <SandpackFileExplorer />
+          <SandpackCodeEditor
+            closableTabs
+            extensions={[autocompletion({ override: [completionSource] }), customCmTheme, keymap.of(searchKeymap)]}
+            showInlineErrors
+            showLineNumbers
+            showTabs
+            wrapContent
+            style={{ height: "calc(100dvh - 4rem)" }}
+          />
+          <SandpackPreview
+            showOpenInCodeSandbox={false}
+            showRefreshButton={false}
+            style={{ height: "calc(100dvh - 4rem)" }}
+            title="Preview"
+          />
+        </SandpackLayout>
+      )}
     </SandpackProvider>
   );
 };
